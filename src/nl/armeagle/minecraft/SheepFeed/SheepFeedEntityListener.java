830846ab1efe5a1e7997a37313a998c233ab322f
ArmEagle <ArmEagle@ArmyBox>
2012-03-04 15:19:39 +0100

    version 1.6 for CB 1.1-R6: - Now lets you feed a sheep by rightclicking also

--- src/nl/armeagle/minecraft/SheepFeed/SheepFeedEntityListener.java
+++ src/nl/armeagle/minecraft/SheepFeed/SheepFeedEntityListener.java
@@ -1,6 +1,7 @@
 package nl.armeagle.minecraft.SheepFeed;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class SheepFeedEntityListener implements Listener{
@@ -48,46 +50,9 @@ public class SheepFeedEntityListener implements Listener{
 			Sheep sheep = (Sheep) target;
 			SheepFeed.debug("Hitting sheep ("+ sheep.getEntityId() +") that had health "+ sheep.getHealth() +" original damage: "+ event.getDamage());
 
-			/* Support for sheared state (setting), using that now.
-			 * If the sheep is sheared, then add to growing (if holding a food item and the sheep isn't growing already. Remove from naked list.
-			 * If not sheared, then add to naked list.
-			 */ 
-			if ( sheep.isSheared() ) {
-				// Sheep is already sheered, adding to growing list if holding a food item and not growing already
-				SheepFeed.debug("Sheep is already sheered");
-				if ( sheepFeedPlugin.isWoolGrowingSheep(sheep) ) {
-					SheepFeed.debug("Sheep is already registered to grow wool");
-					return;
-				}
-				// check whether player has the right item in hand
-				ItemStack heldItemStack = player.getItemInHand();
-				if ( !sheepFeedPlugin.config.isSheepFood(heldItemStack.getTypeId()) ) {
-					// That's not sheep food! 
-					SheepFeed.debug("Sheep would not like that food");
-					return;
-				}
-				// not already scheduled to grow and correct food, remove one item and set growing schedule
-				SheepFoodData foodData = sheepFeedPlugin.config.getFoodData(heldItemStack.getTypeId());
-				SheepFeed.debug(foodData.toString());
-
-				// check for permission
-				if ( !this.sheepFeedPlugin.canFeed(player) ) {
-					player.sendMessage("The sheep doesn't trust you and won't eat your "+ foodData.name +".");
-				} else {
-					// remove the food item
-					SheepFeed.debug(" amount of "+ heldItemStack.getType().toString() +" held: "+ heldItemStack.getAmount());
-					player.sendMessage("The sheep munches on the "+ foodData.name +" you fed it.");
-					// reduce stack by one, but if it's the last item, remove the whole stack
-					if ( heldItemStack.getAmount() == 1 ) {
-						player.getInventory().clear(player.getInventory().getHeldItemSlot());
-					} else {
-						heldItemStack.setAmount(heldItemStack.getAmount()-1);
-					}
-					
-					// schedule growing of wool
-					this.sheepFeedPlugin.scheduleWoolGrowth(sheep, foodData);
-				}
-				
+			this.attemptFeedSheep(player, sheep);
+			
+			if (sheep.isSheared()) {
 				// cancel the damage given
 				event.setDamage(0);
 			}
@@ -112,4 +77,61 @@ public class SheepFeedEntityListener implements Listener{
 		}
 	}
 
+	@EventHandler
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+		// do not act when disabled  TODO, use unregister when available
+		if ( !this.sheepFeedPlugin.isEnabled() ) {
+			return;
+		}
+		
+		Entity entity = event.getRightClicked();
+		
+		if (entity.getType() == EntityType.SHEEP) {
+			this.attemptFeedSheep(event.getPlayer(), (Sheep) entity);
+		}
+		
+	}
+	
+	protected void attemptFeedSheep(Player player, Sheep sheep) {
+		/* Support for sheared state (setting), using that now.
+		 * If the sheep is sheared, then add to growing (if holding a food item and the sheep isn't growing already. Remove from naked list.
+		 * If not sheared, then add to naked list.
+		 */ 
+		if ( sheep.isSheared() ) {
+			// Sheep is already sheered, adding to growing list if holding a food item and not growing already
+			SheepFeed.debug("Sheep is already sheered");
+			if ( sheepFeedPlugin.isWoolGrowingSheep(sheep) ) {
+				SheepFeed.debug("Sheep is already registered to grow wool");
+				return;
+			}
+			// check whether player has the right item in hand
+			ItemStack heldItemStack = player.getItemInHand();
+			if ( !sheepFeedPlugin.config.isSheepFood(heldItemStack.getTypeId()) ) {
+				// That's not sheep food! 
+				SheepFeed.debug("Sheep would not like that food");
+				return;
+			}
+			// not already scheduled to grow and correct food, remove one item and set growing schedule
+			SheepFoodData foodData = sheepFeedPlugin.config.getFoodData(heldItemStack.getTypeId());
+			SheepFeed.debug(foodData.toString());
+
+			// check for permission
+			if ( !this.sheepFeedPlugin.canFeed(player) ) {
+				player.sendMessage("The sheep doesn't trust you and won't eat your "+ foodData.name +".");
+			} else {
+				// remove the food item
+				SheepFeed.debug(" amount of "+ heldItemStack.getType().toString() +" held: "+ heldItemStack.getAmount());
+				player.sendMessage("The sheep munches on the "+ foodData.name +" you fed it.");
+				// reduce stack by one, but if it's the last item, remove the whole stack
+				if ( heldItemStack.getAmount() == 1 ) {
+					player.getInventory().clear(player.getInventory().getHeldItemSlot());
+				} else {
+					heldItemStack.setAmount(heldItemStack.getAmount()-1);
+				}
+				
+				// schedule growing of wool
+				this.sheepFeedPlugin.scheduleWoolGrowth(sheep, foodData);
+			}
+		}
+	}
 }
